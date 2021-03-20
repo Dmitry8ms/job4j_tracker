51ea69acecffb033954154525fe21417ec8b05f7
Dmitry8ms <dshatokhin@yandex.ru>
2021-03-20 19:36:45 +0700

    5. Tracker - хранилище [#464476]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,9 +4,27 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
+        Item item1 = new Item();
+        Item item2 = new Item();
+        Item item3 = new Item();
+        item2.setName("Item #2");
+        item3.setName("Item #3");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String dateTime = item.getTime().format(formatter);
-        System.out.println(dateTime);
+        String dateTime = item1.getTime().format(formatter);
+        //System.out.println(dateTime);
+        item1.setName("Item #1");
+        Tracker database = new Tracker();
+        database.add(item1);
+        database.add(item2);
+        database.add(item3);
+        database.removeById(2);
+        Item[] items = database.findAll();
+        for (Item it : items) {
+            System.out.println(it.getName());
+        }
+        for (Item it : database.findByName("Item #3")) {
+            System.out.println("found by name: " + it.getName());
+        }
+        System.out.println("found by Id: " + database.findById(1).getName());
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,40 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        int realIndex = 0;
+        Item[] arrayCut = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                arrayCut[realIndex++] = items[i];
+            }
+        }
+        arrayCut = Arrays.copyOf(arrayCut, realIndex);
+        return arrayCut;
+    }
+
+    public Item[] findByName(String key) {
+        int realIndex = 0;
+        Item[] arrayCut = findAll();
+        for (int i = 0; i < arrayCut.length; i++) {
+            if (arrayCut[i].getName() == key) {
+                arrayCut[realIndex++] = arrayCut[i];
+            }
+        }
+        arrayCut = Arrays.copyOf(arrayCut, realIndex);
+        return arrayCut;
+    }
+
+    public void removeById(int id) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null && items[i].getId() == id) {
+                System.out.println("found and removed Id: " + items[i].getId() + " for product "
+                                    + items[i].getName());
+                items[i] = null;
+                break;
+            }
+        }
     }
 }
