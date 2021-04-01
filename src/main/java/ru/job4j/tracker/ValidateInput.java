9f6381744e14caa20c0d2d8723db8c8a60d648fe
Dmitry8ms <dshatokhin@yandex.ru>
2021-04-01 18:41:49 +0700

    1. Обеспечить бесперебойную работу приложения Tracker [#464423]

---
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -0,0 +1,27 @@
+package ru.job4j.tracker;
+
+import java.util.Scanner;
+
+public class ValidateInput extends ConsoleInput {
+    private Scanner scanner = new Scanner(System.in);
+
+    public ValidateInput(Output out) {
+        super(out);
+    }
+
+    @Override
+    public int askInt(String question) {
+        boolean invalid = true;
+        int value = -1;
+        do {
+            try {
+                value = super.askInt(question);
+                invalid = false;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Please enter valid data.");
+            }
+        } while (invalid);
+        return value;
+    }
+
+}
