b102b737d15ad108e5e38f5f9bcea31f3a0c79cb
Dmitry8ms <dshatokhin@yandex.ru>
2021-04-24 17:16:36 +0700

    1. Паспорт и жители [#464384]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -14,4 +14,15 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
     }
+
+    @Test
+    public void get() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("11111", "Petr Ivanov");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        office.add(citizen2);
+        Citizen expected = office.get("11111");
+        assertThat(expected.equals(citizen2), is(true));
+    }
 }
