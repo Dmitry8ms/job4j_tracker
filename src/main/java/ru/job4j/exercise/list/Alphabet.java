862741e7d2b543d77e5ca9e4d8f7d51528aa24be
Dmitry8ms <dshatokhin@yandex.ru>
2021-05-16 12:16:14 +0700

    16. Буквы в алфавитном порядке.

--- src/main/java/ru/job4j/exercise/list/Alphabet.java
+++ src/main/java/ru/job4j/exercise/list/Alphabet.java
@@ -5,6 +5,14 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+/* Вам необходимо реализовать метод, который принимает строку, которое является одним словом,
+ без знаков препинания цифр и пробелов. Метод должен вернуть новую строку, в которой все символы
+  исходной строки будут расставлены в алфавитном порядке.
+
+Для разделения строки на подстроки используйте метод toCharArray(), добавьте все элементы в
+ коллекцию List<String>, отсортируйте в порядке возрастания - используйте метод sort, в
+ который передадите  компаратор. Новую строку сформируйте с помощью StringBuilder.*/
+
 public class Alphabet {
     public static String reformat(String s) {
         List<Character> listOfChars = new ArrayList<>();
