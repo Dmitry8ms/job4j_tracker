7f0db99b5f7bf1999ce71dc0d4b4125cdb774c22
Dmitry8ms <dshatokhin@yandex.ru>
2021-05-19 12:49:03 +0700

    6. Функции высшего порядка [#464194]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<>();
@@ -15,12 +16,20 @@ public class PhoneDictionary {
      * @return Список подощедщих пользователей.
      */
     public ArrayList<Person> find(String key) {
+        Predicate<Person> inName = k -> k.getName().contains(key);
+        Predicate<Person> inSurname = k -> k.getSurname().contains(key);
+        Predicate<Person> inAddress = k -> k.getAddress().contains(key);
+        Predicate<Person> inPhone = k -> k.getPhone().contains(key);
+        Predicate<Person> combine = inName.or(inSurname.or(inAddress.or(inPhone)));
         ArrayList<Person> result = new ArrayList<>();
         for (Person p : persons) {
-            if (p.getPhone().contains(key)
+         /*   if (p.getPhone().contains(key)
                     || p.getAddress().contains(key)
                     || p.getName().contains(key)
                     || p.getSurname().contains(key)) {
+                result.add(p);
+            }*/
+            if (combine.test(p)) {
                 result.add(p);
             }
         }
--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -28,4 +28,14 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Stas");
         assertThat(persons.isEmpty(), is(true));
     }
+
+    @Test
+    public void whenFindByPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("534872");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
 }
