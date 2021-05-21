f7f142b7446389c0453f88da417a3d4daab7a8d9
Dmitry8ms <dshatokhin@yandex.ru>
2021-05-21 21:44:58 +0700

    Упражнения. Вычисление значений в отображении если ключа не существует

---
+++ src/main/java/ru/job4j/exercise/map/ComputeIfAbsent.java
@@ -0,0 +1,50 @@
+package ru.job4j.exercise.map;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class ComputeIfAbsent {
+    public static Map<Integer, String> collectData(Map<Integer, String> names, List<User> users) {
+        for (User u : users) {
+            names.computeIfPresent(u.getId(), (key, v) -> u.getName());
+            names.computeIfAbsent(u.getId(), key -> u.getName());
+        }
+        return names;
+    }
+
+    public static class User {
+        private int id;
+        private String name;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            ComputeIfAbsent.User user = (ComputeIfAbsent.User) o;
+            return Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+}
