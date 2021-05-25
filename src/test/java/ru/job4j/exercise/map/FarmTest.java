e1e003259b8bab4a6aad129e374a552def9173dd
Dmitry8ms <dshatokhin@yandex.ru>
2021-05-25 22:16:13 +0700

    Упражнения. Ферма.

---
+++ src/test/java/ru/job4j/exercise/map/FarmTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.exercise.map;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FarmTest {
+
+    @Test
+    public void sumLegs() {
+        Map<Farm.Animal, Integer> data = Map.of(
+                new Farm.Animal("chicken", 2), 5,
+                new Farm.Animal("cow", 4), 2,
+                new Farm.Animal("pig", 4), 8
+        );
+        int rsl = Farm.sumLegs(data);
+        assertThat(rsl, is(50));
+    }
+}
