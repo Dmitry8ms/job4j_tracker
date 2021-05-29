10e4770061f26431a55918d73b68a154f863249a
Dmitry8ms <dshatokhin@yandex.ru>
2021-05-29 16:57:27 +0700

    Упражнения. Optional. Методы isPresent(), isEmpty(), get().

---
+++ src/main/java/ru/job4j/exercise/lambda/OptionalGetAndIsPresent.java
@@ -0,0 +1,22 @@
+package ru.job4j.exercise.lambda;
+
+import java.util.Optional;
+
+public class OptionalGetAndIsPresent {
+    public static int get(int[] data, int el) {
+        Optional<Integer> opt = indexOf(data, el);
+        if (opt.isPresent()) {
+            return opt.get();
+        }
+        return -1;
+    }
+
+    private static Optional<Integer> indexOf(int[] data, int el) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                return Optional.of(i);
+            }
+        }
+        return Optional.empty();
+    }
+}
