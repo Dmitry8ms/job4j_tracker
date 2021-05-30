c44b1501915f70113ac0580d936168fa0f63563d
Dmitry8ms <dshatokhin@yandex.ru>
2021-05-30 17:31:45 +0700

    Упражнения. Понятие редукции. Метод reduce(). Произведение чисел.

---
+++ src/main/java/ru/job4j/exercise/stream/ReduceMethodOneArgument.java
@@ -0,0 +1,10 @@
+package ru.job4j.exercise.stream;
+
+import java.util.List;
+
+public class ReduceMethodOneArgument {
+    public static Integer reduce(List<Integer> list) {
+        return list.stream()
+                .reduce((n1, n2) -> n1 *= n2).get();
+    }
+}
