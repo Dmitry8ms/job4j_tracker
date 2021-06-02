ac842e75e3db6f84f0f43c797e8615a212042174
Dmitry8ms <dshatokhin@yandex.ru>
2021-06-02 16:37:32 +0700

    Упражнения. Промежуточные операции. Метод mapToObj(). Стрим из символов строки.

--- src/main/java/ru/job4j/exercise/stream/CharsMethod.java
+++ src/main/java/ru/job4j/exercise/stream/CharsMethod.java
@@ -3,7 +3,6 @@ package ru.job4j.exercise.stream;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 /* map() возвращает стрим, того же типа (в смысле стрим объектов или стрим примитивов). Если мы
 вызовем map() для стрима объектов, мы получим стрим из объектов. Если мы вызовем map() для стрима
 примитивов IntStream, мы получим стрим IntStream.
@@ -11,13 +10,13 @@ import java.util.stream.Collectors;
 mapToObj(), работает он так же как и map()
 Из символов строки можно получим стрим, используя метод chars() в класс String. Но этот стрим
 возвращает нам IntStream. Ваша задача преобразовать стрим, который вернет chars(), в стрим
-Charater и собрать в список.
+Character и собрать в список.
 IntStream будет содержать коды символы. Чтобы получить символ мы можем сделать так
-Charater c = (char) 91; */
+Character c = (char) 91; */
 
 public class CharsMethod {
     public static List<Character> symbols(String string) {
-        return string.chars().mapToObj(c -> (char)c)
+        return string.chars().mapToObj(c -> (char) c)
                 .collect(Collectors.toList());
     }
 }
